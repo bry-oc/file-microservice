b2c061c976b5f4aa3513cc3d126d481458b0c397
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-06-04 16:53:21 -0400

    connected to mongodb

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 const cors = require('cors');
+const dotenv = require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const PORT = process.env.port || 3001;
 
 const app = express();
+
+mongoose.connect(process.env.MONGO_URI_TEST, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function() {
+    console.log('Connection successful!');
+});
 
 app.listen(PORT, () => {
     console.log(`App is listening on port ${PORT}`);
