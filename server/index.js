1c787be8c0bab969d2f19bf19b3102ca6d22f893
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-06-04 18:10:00 -0400

    added multer file handling

--- server/index.js
+++ server/index.js
@@ -2,10 +2,16 @@ const cors = require('cors');
 const dotenv = require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const multer = require('multer');
+const GridFsStorage = require('multer-gridfs-storage');
 const path = require('path');
 const PORT = process.env.port || 3001;
 
 const app = express();
+
+const storage = new GridFsStorage(process.env.MONGO_URI_TEST);
+
+const upload = multer({storage});
 
 mongoose.connect(process.env.MONGO_URI_TEST, { useNewUrlParser: true, useUnifiedTopology: true });
 
