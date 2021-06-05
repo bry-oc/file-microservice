ece35567d283ed32a1f8d3381ef4a0c6fc9a1842
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-06-05 19:34:12 -0400

    styling

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,8 +1,8 @@
 function Welcome(){
     const baseURL = window.location.href;
     return(
-        <div className="wrapper">
-            <p>Welcome! This is a microservice for files.  You can upload a file and the file name, type, and size will be displayed.  You can also view the file directly at <a href={baseURL+"api/view/"} target="_blank" rel="noopener noreferrer">[baseurl]/api/view/</a>&lt;filename&gt; where &lt;filename&gt; is replaced by the given file name.</p>
+        <div className="wrapper welcome">
+            <p>Welcome! This is a microservice for files.  You can upload a file and the file name, type, and size will be displayed.  You can also view the file directly at <a href={baseURL+"api/view/"} target="_blank" rel="noopener noreferrer">[baseurl]/api/view/&lt;filename&gt;</a> where &lt;filename&gt; is replaced by the given file name.</p>
         </div>
     )
 }
